42054d962c6c459b4216f493b682f627e800ab05
MateoCailletBois <mateocb.desit@gmail.com>
2019-07-24 16:53:12 -0300

    Se crea un proyecto que solo hace titilar un LED
    Es simplemente para probar que anda todo ok. El resultado fue correcto y
    funciono.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,33 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
 #include "stm32f4xx.h"
 #include "stm32f411e_discovery.h"
-			
+#include "FreeRTOS.h"
+#include "task.h"
 
-int main(void)
-{
+
+void vTaskLed( void *pvParameters );
+const TickType_t xDelay250ms = pdMS_TO_TICKS( 1000 );
+Led_TypeDef Led1 = LED3;
+TaskHandle_t Task1;
+
+int main(void) {
+	BSP_LED_Init(LED3);
+	BSP_LED_Init(LED6);
+
+	xTaskCreate( vTaskLed, "Task 1", 100, (void* const)&Led1, 1, &Task1 );
+	vTaskStartScheduler();
 
 	for(;;);
+}
+
+void vTaskLed( void *pvParameters) {
+
+	Led_TypeDef* Led = (Led_TypeDef*) pvParameters;
+
+	while(1) {
+		BSP_LED_On(*Led);
+		vTaskDelay( xDelay250ms );
+
+		BSP_LED_Off(*Led);
+		vTaskDelay( xDelay250ms );
+	}
 }
